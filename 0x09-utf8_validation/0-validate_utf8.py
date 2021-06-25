6a7ad0b7935b78a6cbfc76c257906de702a038c2
I7RANK <franfgh2000@hotmail.com>
2021-06-24 22:17:37 -0500

    uploading all tasks

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module contains the function validUTF8
+"""
+
+def validUTF8(data):
+    """determines if a given data set represents a valid UTF-8 encoding.
+
+    Args:
+        data (list): contain multiple characters
+    """
+    if len(data) > 32:
+        return False
+
+    for i in data:
+        if i > 255:
+            return False
+
+    return True
