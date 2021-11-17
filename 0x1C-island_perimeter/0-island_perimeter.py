2394f4c422c8b2597077accd9fbcda5c03755bbf
I7RANK <franfgh2000@hotmail.com>
2021-11-16 21:46:36 -0500

    Uploading mandatory task

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""This module contains the island_perimeter function
+"""
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid
+    """
+    if grid == []:
+        return
+
+    height = len(grid)
+    width = len(grid[0])
+    perimeter = 0
+
+    i, j = 0, 0
+
+    while i < height:
+        j = 0
+
+        while j < width:
+            if grid[i][j] == 1:
+                perimeter += check_perimeter(grid, i, j)
+
+            j += 1
+
+        i += 1
+
+    return perimeter
+
+
+def check_perimeter(grid, i, j):
+    """Checks if there are water(number 0) around the number 1
+    """
+    perimeter = 0
+
+    if grid[i][j + 1] == 0:
+        perimeter += 1
+
+    if grid[i][j - 1] == 0:
+        perimeter += 1
+
+    if grid[i + 1][j] == 0:
+        perimeter += 1
+
+    if grid[i - 1][j] == 0:
+        perimeter += 1
+
+    return perimeter
---
+++ 0x1C-island_perimeter/README.md
@@ -0,0 +1,5 @@
+# 0x1C. Island Perimeter
+[Link to the project](https://intranet.hbtn.io/projects/496)
+
+## Tasks - Island Perimeter
+Create a function def island_perimeter(grid): that returns the perimeter of the island described in grid:
