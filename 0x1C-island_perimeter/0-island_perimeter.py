8414353ebfe6c5ece0ede218f581c799953b4f9f
I7RANK <franfgh2000@hotmail.com>
2021-11-18 23:47:06 -0500

    Fixing styles

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -42,13 +42,11 @@ def check_perimeter(grid, i, j):
     else:
         perimeter += 1
 
-
     if j - 1 >= 0:
         if grid[i][j - 1] == 0:
             perimeter += 1
     else:
         perimeter += 1
-
 
     if i + 1 < height:
         if grid[i + 1][j] == 0:
@@ -56,12 +54,10 @@ def check_perimeter(grid, i, j):
     else:
         perimeter += 1
 
-
     if i - 1 >= 0:
         if grid[i - 1][j] == 0:
             perimeter += 1
     else:
         perimeter += 1
-
 
     return perimeter
