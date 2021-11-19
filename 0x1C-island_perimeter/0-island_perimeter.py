aeacb18e322e2ef1acda7feb11f5cd79596aebf4
I7RANK <franfgh2000@hotmail.com>
2021-11-18 23:45:22 -0500

    Adding some conditionals to avoid IndexError: list index out of range

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -32,18 +32,36 @@ def island_perimeter(grid):
 def check_perimeter(grid, i, j):
     """Checks if there are water(number 0) around the number 1
     """
+    height = len(grid)
+    width = len(grid[0])
     perimeter = 0
 
-    if grid[i][j + 1] == 0:
+    if (j + 1) < width:
+        if grid[i][j + 1] == 0:
+            perimeter += 1
+    else:
         perimeter += 1
 
-    if grid[i][j - 1] == 0:
+
+    if j - 1 >= 0:
+        if grid[i][j - 1] == 0:
+            perimeter += 1
+    else:
         perimeter += 1
 
-    if grid[i + 1][j] == 0:
+
+    if i + 1 < height:
+        if grid[i + 1][j] == 0:
+            perimeter += 1
+    else:
         perimeter += 1
 
-    if grid[i - 1][j] == 0:
+
+    if i - 1 >= 0:
+        if grid[i - 1][j] == 0:
+            perimeter += 1
+    else:
         perimeter += 1
+
 
     return perimeter
