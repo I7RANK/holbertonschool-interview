32448f7cbd3eb4ee6c6520fef93f52860666bd7b
I7RANK <franfgh2000@hotmail.com>
2021-12-19 07:36:30 -0500

    Uploading mandatory task

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -4,7 +4,7 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=''):
+def count_words(subreddit, word_list, hot_list=[], after=''):
     """ returns the first """
     my_header = {'user-agent': 'i7RANK'}
     par = {'after': after}
@@ -12,15 +12,36 @@ def recurse(subreddit, hot_list=[], after=''):
         subreddit,
         after
     )
+
     rq = requests.get(
         url,
         headers=my_header,
         allow_redirects=False,
     )
+
     if rq.status_code == 200:
         hot_list += rq.json().get("data", {}).get("children", [])
         affffter = rq.json().get("data", {}).get("after", None)
+
         if affffter:
-            return recurse(subreddit, hot_list=hot_list, after=affffter)
+            return count_words(
+                subreddit,
+                word_list,
+                hot_list=hot_list,
+                after=affffter
+            )
         else:
-            return hot_list
+            word_dict = {
+                word_list[i].lower(): 0 for i in range(len(word_list))
+            }
+
+            for i in hot_list:
+                title = i.get('data').get('title').lower().split()
+
+                for k in word_list:
+                    if k.lower() in title:
+                        word_dict[k.lower()] += 1
+
+            for k in sorted(word_dict.keys()):
+                if word_dict[k] > 0:
+                    print('{}: {}'.format(k, word_dict[k]))
