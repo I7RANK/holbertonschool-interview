ae97f61cf18972b638e958c7896cc97cf698f8ea
I7RANK <franfgh2000@hotmail.com>
2021-12-19 06:29:41 -0500

    Uploading mandatory task

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,53 +1,26 @@
 #!/usr/bin/python3
-"""This module contains the count_words function"""
+""" this module contain top_ten function """
 
 import requests
 
 
-def print_greater_than_zero(word_counter):
-    """print all keys and values grater than zero
-    """
-    keys = list(word_counter.keys())
-
-    keys.sort()
-
-    for k in keys:
-        v = word_counter[k]
-        if v > 0:
-            print("{}: {}".format(k, v))
-
-
-def count_words(subreddit, word_list, after="", word_counter={}):
-    """queries the Reddit API,
-    """
-    if after == "":
-        for i in range(len(word_list)):
-            word_list[i] = word_list[i].lower()
-            word_counter[word_list[i]] = 0
-
-    if after is None:
-        print_greater_than_zero(word_counter)
-        return
-
-    headers = {"user-agent": "myapp/v0.1"}
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?after={after}"
-
-    try:
-        res = requests.get(url, headers=headers)
-
-        json_data = res.json().get("data")
-        after = json_data.get("after")
-
-        childrens = json_data.get("children")
-
-        for children in childrens:
-            title = children.get("data").get("title").split()
-
-            for j in title:
-                j = j.lower()
-                if j in word_counter.keys():
-                    word_counter[j] += 1
-
-        count_words(subreddit, word_list, after, word_counter)
-    except Exception:
-        return
+def recurse(subreddit, hot_list=[], after=''):
+    """ returns the first """
+    my_header = {'user-agent': 'i7RANK'}
+    par = {'after': after}
+    url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(
+        subreddit,
+        after
+    )
+    rq = requests.get(
+        url,
+        headers=my_header,
+        allow_redirects=False,
+    )
+    if rq.status_code == 200:
+        hot_list += rq.json().get("data", {}).get("children", [])
+        affffter = rq.json().get("data", {}).get("after", None)
+        if affffter:
+            return recurse(subreddit, hot_list=hot_list, after=affffter)
+        else:
+            return hot_list
